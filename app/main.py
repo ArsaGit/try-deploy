c5b22dd6d9e29ebb32c664ab84ef2b559d6a9818
arsalanov <arsalanov@intersvyaz.net>
2023-11-07 00:48:46 +0500

    added route to test ci

--- app/main.py
+++ app/main.py
@@ -7,3 +7,8 @@ app = FastAPI()
 @app.get("/", status_code=status.HTTP_200_OK)
 def read_root():
     return {"text": "HI"}
+
+
+@app.get("/info", status_code=status.HTTP_200_OK)
+def read_info():
+    return {"info": "testing deployment"}
--- app/test_main.py
+++ app/test_main.py
@@ -9,3 +9,9 @@ def test_read_main():
     response = client.get("/")
     assert response.status_code == 200
     assert response.json() == {"text": "HI"}
+
+
+def test_read_info():
+    response = client.get("/info")
+    assert response.status_code == 200
+    assert response.json() == {"info": "testing deployment"}
